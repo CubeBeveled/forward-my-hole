a16040c7bf42a82d568a21fdb671c2354a77c5e0
CubeBeveled <cubebeveled@gmail.com>
2025-04-02 21:24:16 +0200

    WE ARE SO CLOSE

--- client/index.js
+++ client/index.js
@@ -1,31 +1,12 @@
 const WebSocket = require("ws");
 const color = require("colors");
-const dns = require("dns");
-const { Packet, UDPClient } = require("dns2");
+const { UDPClient, Packet } = require("dns2");
 
 const sleep = (ms) => {
   return new Promise((r) => {
     setTimeout(r, ms);
   });
 };
-
-const dnsCallback = (err, addresses) => {
-  if (err) {
-    console.log(color.yellow(`Error: Pihole did not respond. Using fallback (${color.white(process.env.FALLBACK_DNS)})`));
-
-    dns.setServers([process.env.FALLBACK_DNS]);
-
-    dns.resolve4(domain, dnsErrorCallback);
-  } else resolve(addresses);
-};
-
-const dnsErrorCallback = (err, addresses) => {
-  if (err) {
-    console.log(color.red("Error: Fallback DNS failed:"), err);
-
-    resolve(null);
-  } else resolve(addresses);
-}
 
 require("dotenv").config();
 
@@ -37,7 +18,7 @@ function connectToHub() {
   restarting = false;
 
   let socket = new WebSocket(process.env.HUB_ADDRESS);
-
+  Packet.TYPE
   socket.on("open", () => {
     socket.send(process.env.ACCESS_CODE);
     console.log(color.green("Connected to hub"));
@@ -50,16 +31,20 @@ function connectToHub() {
     const start = Date.now();
 
     try {
-      resolved = await resolve(data.name, data.type, data.class);
+      resolved = await UDPClient({
+        dns: process.env.PIHOLE_ADDRESS,
+      })(data.name, data.type, data.class);
+
       socket.send(JSON.stringify({ domain: data.name, resolved }));
     } catch (err) {
-      console.log(color.red("Error resolving"), strMsg)
+      console.log(color.red("Error resolving"), data.name)
       console.log(err);
+      return;
     }
 
     const end = Date.now();
 
-    console.log(`Resolved ${color.white(strMsg)} (${color.white(end - start)}ms)`);
+    console.log(color.green(`Resolved ${color.white(data.name)} -> ${color.white(resolved.answers)} (${color.white(`${end - start}ms`)})`));
   });
 
   socket.on("error", (err) => {
@@ -81,96 +66,41 @@ function connectToHub() {
   });
 }
 
-async function resolve(domain, type, cclass) {
-  const resolved = [];
-
-  let data;
-  let extra = {}
-
-  if (type === Packet.TYPE.A) {  // 'A' Record (IPv4 Address)
-    data = await resolveA(domain)
-    extra.address = data;
-  } else if (type === Packet.TYPE.AAAA) {  // 'AAAA' Record (IPv6 Address)
-    data = await resolveAAAA(domain)
-    extra.address = data;
-  } else if (type === Packet.TYPE.MX) {  // 'MX' Record (Mail Exchange)
-    // Return an MX record for the domain
-    data = await resolveMX(domain);
-
-    resolver.push({
-      name: domain,
-      type: Packet.TYPE.MX, // MX record type
-      class: Packet.CLASS.IN,
-      ttl: 60,
-      priority: 10,  // Priority for mail exchange
-      exchange: 'mail.example.com',  // Mail server domain
-    });
-
-    extra.exchange = data;
-    extra.priority = data;
-  } else if (type === Packet.TYPE.CNAME) {  // 'CNAME' Record (Canonical Name)
-    // Return a CNAME record for the domain
-    data = await resolveA(domain);
-
-    resolver.push({
-      name: domain,
-      type: Packet.TYPE.CNAME, // CNAME record type
-      class: Packet.CLASS.IN,
-      ttl: 60,
-      target: 'example.com',
-    });
-  } else if (type === Packet.TYPE.TXT) {  // 'TXT' Record (Text)
-    data = await resolveA(domain);
-
-    resolver.push({
-      name: domain,
-      type: Packet.TYPE.TXT,
-      class: Packet.CLASS.IN,
-      ttl: 60,
-      text: ,  // Example text (SPF)
-    });
-  }
-
-  resolved.push(data.map((d) => {
-    d.name = data.address;
-    d.type = type;
-    d.class = cclass;
-    d.ttl = data.ttl;
-  }));
-}
-
-function resolveA(domain) {
-  return new Promise((resolve, reject) => {
-
-    dns.setServers([process.env.PIHOLE_ADDRESS]);
-
-    dns.resolve4(domain, { ttl: true }, dnsCallback);
+// finish this function and
+// put this instead of data.type
+/*
+resolved = await UDPClient({
+        dns: process.env.PIHOLE_ADDRESS,
+      })(data.name, numberToString(data.type), data.class);
+ */
+function numberToString(number) {
+  const rawTypes = Object.entries({
+    A: 0x01,
+    NS: 0x02,
+    MD: 0x03,
+    MF: 0x04,
+    CNAME: 0x05,
+    SOA: 0x06,
+    MB: 0x07,
+    MG: 0x08,
+    MR: 0x09,
+    NULL: 0x0a,
+    WKS: 0x0b,
+    PTR: 0x0c,
+    HINFO: 0x0d,
+    MINFO: 0x0e,
+    MX: 0x0f,
+    TXT: 0x10,
+    AAAA: 0x1c,
+    SRV: 0x21,
+    EDNS: 0x29,
+    SPF: 0x63,
+    AXFR: 0xfc,
+    MAILB: 0xfd,
+    MAILA: 0xfe,
+    ANY: 0xff,
+    CAA: 0x101
   });
-}
 
-function resolveAAAA(domain) {
-  return new Promise((resolve, reject) => {
-
-    dns.setServers([process.env.PIHOLE_ADDRESS]);
-
-    dns.resolve6(domain, { ttl: true }, dnsCallback);
-  });
-}
-
-function resolveMX(domain) {
-  return new Promise((resolve, reject) => {
-
-    dns.setServers([process.env.PIHOLE_ADDRESS]);
-
-    dns.resolveMX(domain, {}, dnsCallback);
-  });
-}
-
-function resolveTXT(domain) {
-  return new Promise((resolve, reject) => {
-
-    dns.setServers([process.env.PIHOLE_ADDRESS]);
-
-    dns.resolveTXT(domain, { ttl: true }, dnsCallback);
-  });
+  const finalMap = new Map();
 }
--- hub/index.js
+++ hub/index.js
@@ -1,10 +1,24 @@
 const express = require("express")();
-const dns2 = require("dns2");
 const WebSocket = require("ws");
+const dns2 = require("dns2");
+const color = require("colors");
 
 require("dotenv").config();
+class BoundedMap extends Map {
+  constructor(maxSize) {
+    super();
+    this.maxSize = maxSize;
+  }
+
+  set(key, value) {
+    super.set(key, value);
+
+    if (this.size > this.maxSize) this.delete(this.keys().next().value);
+  }
+}
 
 let globalWS;
+let cache = new BoundedMap(process.env.CACHE_SIZE | 20);
 
 startWS();
 startDNS();
@@ -14,7 +28,7 @@ async function startWS() {
   const wss = new WebSocket.Server({ server });
 
   wss.on("connection", (ws, req) => {
-    console.log("Pihole connected");
+    console.log(color.blue("Pihole connected"));
     let receivedAccessCode = false;
 
     ws.on("message", (msg) => {
@@ -23,7 +37,7 @@ async function startWS() {
           receivedAccessCode = true;
           globalWS = ws;
 
-          console.log("Pihole authenticated");
+          console.log(color.green("Pihole authenticated"));
         } else {
           ws.close(1002, "Access codes do not match.");
           return;
@@ -32,53 +46,75 @@ async function startWS() {
     });
   });
 
-  server.listen(process.env.WSS_PORT, () => console.log(`WSS is running on port ${process.env.WSS_PORT}`));
+  server.listen(process.env.WSS_PORT, () => console.log(color.green(`WSS is running on port ${color.white(process.env.WSS_PORT)}`)));
 }
 
 async function startDNS() {
   const server = dns2.createServer({
     udp: true,
     handle: (request, send, rinfo) => {
-      const response = dns2.Packet.createResponseFromRequest(request);
+      const start = Date.now();
+      let response = dns2.Packet.createResponseFromRequest(request);
+
+      if (!globalWS) {
+        send(response);
+        return;
+      }
 
       const msgCallback = (msg) => {
         const data = JSON.parse(msg);
 
-        if (data.domain == currentQuestion.name) {
-          data.resolved.forEach(ip => response.answers.push(ip));
-          ws.removeListener('message', msgCallback);
+        if (data.domain == question.name) {
+          response.answers = data.resolved.answers;
+          globalWS.removeListener("message", msgCallback);
+
+          console.log("Out:", request);
+          send(response);
         }
       }
 
-      for (const currentQuestion of request.questions) {
-        globalWS.send(currentQuestion.name);
+      //console.log(request.questions)
+      const question = request.questions[0]
 
-
+      const cacheResponse = cache.get(question.name);
+      if (cacheResponse == null) {
+        globalWS.send(JSON.stringify(question));
         globalWS.on("message", msgCallback);
+      } else {
+        response.answers = cacheResponse;
+
+        send(response);
       }
 
-      send(response);
+      const end = Date.now();
 
-      console.log(`Resolved ${request.questions.join(", ")}`)
+      cache.set(question.name, response.answers);
+
+      //console.log(color.green(`Resolved ${request.questions.map((q) => q.name).join(", ")} (${color.white(end - start)}ms)`)); // Uncomment this if you want domains in the logs
+      console.log(color.green(`Resolved in ${color.white(`${end - start}ms`)}`));
     }
   });
 
+  server.on("request", (request, response, rinfo) => {
+    console.log("In:", request);
+  });
+
   server.on("requestError", (error) => {
-    console.log("Client sent an invalid request", error);
+    console.log(color.red("Client sent an invalid request"), error);
   });
 
   server.on("close", () => {
-    console.log("DNS server closed");
+    console.log(color.yellow("DNS server closed"));
   });
 
   server.on("listening", () => {
-    console.log("DNS server started on port", process.env.DNS_PORT)
+    console.log(color.green("DNS server started on port"), process.env.DNS_PORT);
   })
 
   server.listen({
     udp: {
       port: process.env.DNS_PORT,
-      address: "127.0.0.1",
+      address: "0.0.0.0",
       type: "udp4",
     },
   });
